b9cc99d5a7c49379eec0f4ad8cc3f358308ad31a
yosefmelamed <yosefmelamed112@gmail.com>
2026-04-16 09:12:54 -0600

    removed comment from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  //test
   return (
     <html lang="en">
       <body>{children}</body>
