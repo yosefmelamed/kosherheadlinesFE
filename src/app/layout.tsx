0ab3bcaf73c6f2985b121cbafd4c99253f05112c
yosefmelamed <yosefmelamed112@gmail.com>
2026-04-16 09:09:29 -0600

    added comment to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  //test
   return (
     <html lang="en">
       <body>{children}</body>
