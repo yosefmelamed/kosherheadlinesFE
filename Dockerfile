46b8b1111aae58081c80d763361f5b155e4923f0
yosefmelamed <yosefmelamed112@gmail.com>
2026-04-17 08:59:43 -0600

    added be url2

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ WORKDIR /app
 COPY package*.json ./
 RUN npm install
 ARG NEXT_PUBLIC_API_URL
-ENV NEXT_PUBLIC_API_URL=$NEXT_PUBLIC_API_URL
+ENV NEXT_PUBLIC_API_URL = 'https://kosherheadlinesbe.onrender.com';
 COPY . .
 RUN npm run build
 EXPOSE 3000
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001',
+    NEXT_PUBLIC_API_URL: 'https://kosherheadlinesbe.onrender.com',
   },
 };
 
